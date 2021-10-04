0dd0657cf6ca4804211d4623484010585686939a
HerrWal <85037202+HerrWal@users.noreply.github.com>
2021-10-03 21:28:04 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,12 +29,12 @@ function showPage (list, page) {
          li.classList.add('student-item', 'cf');
          li.innerHTML = `
          <div class="student-details">
-         <img class="avatar" src="${data[i].picture.large}" alt="Profile Picture">
-         <h3>${data[i].name.first} ${data[i].name.last}</h3>
-         <span class="email">${data[i].email}</span>
+         <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+         <h3>${list[i].name.first} ${list[i].name.last}</h3>
+         <span class="email">${list[i].email}</span>
        </div>
        <div class="joined-details">
-         <span class="date">Joined ${data[i].registered.date}</span>
+         <span class="date">Joined ${list[i].registered.date}</span>
        </div>
          `;
          studentList.append(li);
@@ -48,9 +48,11 @@ This function will create and insert/append the elements needed for the paginati
 */
 
 function addPagination (list) {
-   const paginationBtns = list.length / 9;
+   const paginationBtns = Math.ceil(list.length / 9);
    const linkList = document.querySelector('.link-list');
    linkList.innerHTML = '';
+   const pagination = document.querySelector('.pagination');
+   pagination.removeAttribute('style');
 
    for (let i= 0; i < paginationBtns; i++) {
       let page = 1 + i;
@@ -69,18 +71,14 @@ function addPagination (list) {
       const eventTarget = event.target;
       if (eventTarget.type === 'button') {
          for (const button of buttons) {
-            button.classList.remove('active')
-            eventTarget.classList.add('active')
+            button.classList.remove('active');
+            eventTarget.classList.add('active');
           }         
          const pageNumber = eventTarget.textContent;
-         showPage (list, pageNumber)
+         showPage (list, pageNumber);
       }
    });
 }
-
-
-// Call functions
- 
 
 /*
 Extra Credit: Search bar
@@ -92,23 +90,17 @@ function createElement(elementName) {
 }
 
 const header = document.querySelector('.header');
-
 const label = createElement('label');
 label.setAttribute('for', 'search');
 label.classList.add('student-search');
-
 const searchBar = createElement('input');
 searchBar.id = 'search';
 searchBar.placeholder = 'Search by name...';
-
 const searchButton = createElement('button');
 searchButton.type = 'button';
-
 const img = createElement('img');
 img.src = 'img/icn-search.svg';
 img.alt = 'Search icon';
-
-
 const span = createElement('span');
 span.textContent = 'Search by name';
 
@@ -116,26 +108,49 @@ searchButton.appendChild(img);
 label.append(span, searchBar, searchButton);
 header.insertAdjacentElement('beforeend', label);
 
-const search = document.getElementById('search');
-const studentItems = document.getElementsByClassName('student-details');
+// The function searchName to perform a search.
 
-// The function searchName to perform a search accepting two parameters
-// searchInput and names.
-function searchName (searchInput, names) {
-   for (let i = 0; i < names.length; i++) {
-      names[i].parentElement.style.display = 'none';
-      if (!(searchInput.value.length === 0) && names[i].children[1].textContent.toLowerCase().includes(searchInput.value.toLowerCase())) {
-         names[i].parentElement.removeAttribute('style');
-      } else if (searchInput.value.length === 0) {
-         showPage (data, 1);
-      }  
+
+function searchName () {
+const name = searchBar.value;
+const searchResults = [];
+
+   for (let i = 0; i < data.length; i++) {
+      const matchingName = `${data[i]['name']['first']} ${data[i]['name']['last']}`.toLocaleLowerCase();
+      if (matchingName.includes(name.toLocaleLowerCase())) {
+         searchResults.push(data[i]);
+         showPage(searchResults, 1);
+         addPagination(searchResults);
+      }
+
+      if (searchResults == 0) {
+         const ul = document.querySelector('.student-list');
+         ul.innerHTML = `<li class="no-results"> 
+                                    No Results...
+                                 </li>`;
+         const pagination = document.querySelector('.pagination');
+         pagination.style.display = 'none';
+      }   
    }
 }
-showPage (data, 1)
 
 // keyup event listener to list filters in real-time as the user types
 searchBar.addEventListener('keyup', () => {
-   addPagination (searchName(search, studentItems))
-   
-   // addPagination (studentList)  
+   if (searchBar.value === '') {
+      showPage(data, 1);
+      addPagination(data);
+   } else {
+      searchName();
+   }
 });
+
+searchBar.addEventListener('submit', () => {
+   searchName();
+})
+
+searchButton.addEventListener('click', () => {
+   searchName();
+})
+
+showPage(data, 1);
+addPagination(data);
